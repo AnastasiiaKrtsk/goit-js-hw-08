40affacc2853164c58a6249d1df578814dfac7cf
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-09-05 10:37:59 +0300

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+//todo 1
+function createGallery(items) {
+  const galleryList = document.querySelector('.gallery');
+  const galleryMarkup = items
+    .map(
+      item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+      </a>
+    </li>
+  `
+    )
+    .join('');
+  galleryList.innerHTML = galleryMarkup;
+}
+createGallery(galleryItems);
 console.log(galleryItems);
+
+//todo 2
+document.querySelector('.gallery').addEventListener('click', event => {
+  event.preventDefault();
+  const target = event.target;
+  const imageElement = target.closest('.gallery__image');
+  if (imageElement) {
+    const largeImageURL = imageElement.dataset.source;
+    const instance = basicLightbox.create(`
+      <img src="${largeImageURL}" alt="Large Image" />
+    `);
+    instance.show();
+  }
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
 
+const player = new Vimeo('vimeo-player');
+
+function saveTimeToLocalStorage(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+function loadTimeFromLocalStorage() {
+  return parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+}
+
+player.ready().then(() => {
+  const savedTime = loadTimeFromLocalStorage();
+  player.setCurrentTime(savedTime);
+});
+player.on('timeupdate', event => {
+  const currentTime = event.seconds;
+  saveTimeToLocalStorage(currentTime);
+});
